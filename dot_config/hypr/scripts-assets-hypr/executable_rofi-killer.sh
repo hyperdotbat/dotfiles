3ccaa04f919bea6c5c5a2f2aab826237decc20aa
hyperdotbat <haipadev@gmail.com>
2026-01-07 17:44:19 +0100

    fix rofi killer & missing aliases

--- dot_config/hypr/scripts-assets-hypr/executable_rofi-killer.sh
+++ dot_config/hypr/scripts-assets-hypr/executable_rofi-killer.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")" || exit 1
 
-# ~~this requires `rofi/rofi-process-killer.sh` to either be symlinked or moved to `.local/bin` or whatever is on $PATH~~
-# this requires dotfiles to be under `~` (i mean where else would u have them lol)
-rofi -modi "processes:~/dotfiles/rofi/rofi-process-killer.sh" -show processes -theme-str 'element-text {wrap: true;} window {width: 95%;}'
+rofi -modi "processes:~/.config/rofi/rofi-process-killer.sh" -show processes -theme-str 'element-text {wrap: true;} window {width: 95%;}'
---
+++ executable_dot_aliases-arch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+alias yay='paru'
+
+alias list_installed_packages='~/scripts/list_installed_packages.sh'
+alias list_installed_packages_by_size='~/scripts/list_installed_packages_by_size.sh'
+alias list_last_installed_packages="grep '\[ALPM\] installed' /var/log/pacman.log | sort | tail -n 20"
+
+btrfs-snapshot(){
+	local suffix=$1
+	local snapshots_dir="/.snapshots"
+	sudo mkdir -p "$snapshots_dir"
+	if [ -z "$suffix" ]; then
+		sudo btrfs subvolume snapshot / "$snapshots_dir/@-$(date +%Y-%m-%d)"
+	else
+		sudo btrfs subvolume snapshot / "$snapshots_dir/@-$(date +%Y-%m-%d)-$suffix"
+	fi
+
+	update-grub
+}
+
+alias reflector-update='sudo reflector --country Poland,Germany --latest 10 --sort rate --save /etc/pacman.d/mirrorlist'
+alias update-clean='sudo pacman -Syu && date +%s > $HOME/.last_arch_update'
+#alias update='btrfs-snapshot "BeforeUpdate" && reflector-update && update-clean && update-grub'
+alias update='tmux new-session -A -s update bash -ic "btrfs-snapshot \"BeforeUpdate\" && reflector-update && update-clean && update-grub; exec bash"'
+
+alias hyprconfig="cd ~/.config/hypr && code ." #ranger" #"v ~/.config/hypr/hyprland.conf"
+alias hyprcfg="hyprconfig"
+alias dotfilescfg="cd ~/dotfiles && code ."
+
